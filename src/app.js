9155643da5cc16ab0ded0357948d4e5a7e836768
kapilcharan <108183192+kapilcharan@users.noreply.github.com>
2024-03-12 23:54:35 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,8 +17,6 @@ app.use('/public',express.static("public"))
 app.set('view engine','hbs')
 app.set("views","views")
 
-// mongodb connection code 
-// mongoose.connect("mongodb://127.0.0.1:27017/pawan",{useNewUrlParser:true,useUnifiedTopology:true},).then(()=> {console.log('db connected');}).catch((err)=>{console.log(err);});
 
 
 
